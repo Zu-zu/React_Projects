97e7a3380ab2043f94d3cc5bfd2235d33bb7db7b
Zu-zu <zuza.nagadowska@gmail.com>
2022-03-26 20:03:55 +0100

    created main React components for the page, as well as added some rudementary CSS with Flexbox and such

--- Project1/index.js
+++ Project1/index.js
@@ -1,24 +1,49 @@
-//import React from "react"
-//import ReactDOM from "react-dom"
+function Reasons() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
 
+function MainContent() {
+    return(
+        <div>
+            <h3>Reasons I am happy to learn react</h3>
+            <ul>
+                <li>It looks cool</li>
+                <li>it's just a hobby bro</li>
+                <li>It is quite popular and might be useful in the future</li>
+            </ul>
+        </div>
+    )
+}
 
-const page = (
-    <div>
-        <img src="./React-icon.png" width="50px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+function Header() {
+    return(
+        <nav className="nav-bar">
+            <img src="./React-icon.png" className="nav-logo" />
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+function Footer() {
+    return(
+        <div className="footer" >
+            <p className="footer-text">C 2022 Nagadowska Development. All rights reserved.</p>
+        </div>
+    )
+}
 
 
 ReactDOM.render(
-    page,
+    <Reasons />,
     document.getElementById("root")
 )
-
